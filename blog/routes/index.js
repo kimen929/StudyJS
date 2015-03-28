56cfa731ca4d6962d09631af31d298f7c7e2328e
kimen929 <kimen929@gmail.com>
2015-03-28 17:33:35 +0900

    add search function to blog

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -311,6 +311,21 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/search', function (req, res) {
+    Post.search(req.query.keyword, function (err, posts) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('/');
+      }
+      res.render('search', {
+        title: "SEARCH:" + req.query.keyword,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
 
   function checkLogin(req, res, next) {
     if (!req.session.user) {
