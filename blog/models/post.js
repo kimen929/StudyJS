5018b54c61171cedc37e99adadd5201a44928f75
kimen929 <kimen929@gmail.com>
2015-03-29 17:13:15 +0900

    add reprint function to blog

--- blog/models/post.js
+++ blog/models/post.js
@@ -31,9 +31,10 @@ Post.prototype.save = function(callback) {
       tags: this.tags,
       post: this.post,
       comments: [],
+      reprint_info: {},
       pv: 0
   };
-
+ 
     //Open DB
   mongodb.open(function (err, db) {
     if (err) {
@@ -207,33 +208,63 @@ Post.update = function(name, day, title, post, callback) {
   });
 };
 
-
 //Delete post
 Post.remove = function(name, day, title, callback) {
-  //open DB
   mongodb.open(function (err, db) {
     if (err) {
       return callback(err);
     }
-    //read post collection
     db.collection('posts', function (err, collection) {
       if (err) {
         mongodb.close();
         return callback(err);
       }
-      //Search for the post
-      collection.remove({
+      collection.findOne({
         "name": name,
         "time.day": day,
         "title": title
-      }, {
-        w: 1
-      }, function (err) {
-        mongodb.close();
+      }, function (err, doc) {
         if (err) {
+          mongodb.close();
           return callback(err);
         }
-        callback(null);
+        var reprint_from = "";
+        if (doc.reprint_info.reprint_from) {
+          reprint_from = doc.reprint_info.reprint_from;
+        }
+        if (reprint_from != "") {
+          collection.update({
+            "name": reprint_from.name,
+            "time.day": reprint_from.day,
+            "title": reprint_from.title
+          }, {
+            $pull: {
+              "reprint_info.reprint_to": {
+                "name": name,
+                "day": day,
+                "title": title
+            }}
+          }, function (err) {
+            if (err) {
+              mongodb.close();
+              return callback(err);
+            }
+          });
+        }
+
+        collection.remove({
+          "name": name,
+          "time.day": day,
+          "title": title
+        }, {
+          w: 1
+        }, function (err) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(null);
+        });
       });
     });
   });
@@ -354,6 +385,83 @@ Post.search = function(keyword, callback) {
          return callback(err);
         }
         callback(null, docs);
+      });
+    });
+  });
+};
+
+
+//转载一篇文章
+Post.reprint = function(reprint_from, reprint_to, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //找到被转载的文章的原文档
+      collection.findOne({
+        "name": reprint_from.name,
+        "time.day": reprint_from.day,
+        "title": reprint_from.title
+      }, function (err, doc) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+
+        var date = new Date();
+        var time = {
+            date: date,
+            year : date.getFullYear(),
+            month : date.getFullYear() + "-" + (date.getMonth() + 1),
+            day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+            minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
+            date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+        }
+
+        delete doc._id;//注意要删掉原来的 _id
+
+        doc.name = reprint_to.name;
+        doc.head = reprint_to.head;
+        doc.time = time;
+        doc.title = (doc.title.search(/[转载]/) > -1) ? doc.title : "[转载]" + doc.title;
+        doc.comments = [];
+        doc.reprint_info = {"reprint_from": reprint_from};
+        doc.pv = 0;
+
+        //更新被转载的原文档的 reprint_info 内的 reprint_to
+        collection.update({
+          "name": reprint_from.name,
+          "time.day": reprint_from.day,
+          "title": reprint_from.title
+        }, {
+          $push: {
+            "reprint_info.reprint_to": {
+              "name": doc.name,
+              "day": time.day,
+              "title": doc.title
+          }}
+        }, function (err) {
+          if (err) {
+            mongodb.close();
+            return callback(err);
+          }
+        });
+
+        //将转载生成的副本修改后存入数据库，并返回存储后的文档
+        collection.insert(doc, {
+          safe: true
+        }, function (err, post) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(err, post[0]);
+        });
       });
     });
   });
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -289,6 +289,29 @@ module.exports = function(app) {
   });
 
 
+app.get('/reprint/:name/:day/:title', checkLogin);
+app.get('/reprint/:name/:day/:title', function (req, res) {
+  Post.edit(req.params.name, req.params.day, req.params.title, function (err, post) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect(back);
+    }
+    var currentUser = req.session.user,
+        reprint_from = {name: post.name, day: post.time.day, title: post.title},
+        reprint_to = {name: currentUser.name, head: currentUser.head};
+    Post.reprint(reprint_from, reprint_to, function (err, post) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('back');
+      }
+      req.flash('success', 'Reprint success!');
+      var url = encodeURI('/u/' + post.name + '/' + post.time.day + '/' + post.title);
+      //Redirect to the direction url
+      res.redirect(url);
+    });
+  });
+});
+
   app.post('/u/:name/:day/:title', function (req, res) {
     var date = new Date(),
         time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
